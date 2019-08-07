a12bdea71efa369c9288ce1e8946ebf71d2405d3
Kavir Bheeroo <raiuniverse@yahoo.com>
2019-08-07 23:27:38 +0200

    Added an API client library

---
+++ src/Gateway/Gateway.Client/Handlers/ValidateAuthorizationHeaderHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gateway.Client.Handlers
+{
+    public class ValidateAuthorizationHeaderHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.Headers.Contains("Authorization"))
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("You must supply an API key header called 'Authorization'")
+                };
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ src/Gateway/Gateway.Client/Interfaces/IGatewayHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using Gateway.Contracts.Public.Models;
+using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace Gateway.Client.Interfaces
+{
+    public interface IGatewayHttpClient
+    {
+        [Get("/api/payments")]
+        Task<PaymentResponse> GetPaymentAsync(Guid paymentId, [Header("Authorization")] string authorization);
+
+        [Post("/api/payments")]
+        Task<PaymentResponse> CreatePaymentAsync(PaymentRequest request, [Header("Authorization")] string authorization);
+    }
+}
